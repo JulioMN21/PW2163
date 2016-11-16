0398bcc803dc2b5cc47469c7431592a7637e88e1
CCDM15 <ccdm15@iMac-de-CCDM15.local>
2016-11-16 12:04:22 -0700

    Uso de JQUERY con nombre e imagen

--- js/eventos.js
+++ js/eventos.js
@@ -1,11 +1,20 @@
 //DOM = Modelo de objetos del documento
+
 var inicio = function() //main
 {
-	var dameclic = function()
+	var dameClic = function()
 	{
-		alert("Le di clic a un bóton");
+		$.ajax({
+  		url: 'https://randomuser.me/api/',
+  		dataType: 'json',
+  		success: function(data) {
+  			$("#txtNombre").val(data.results[0].name.first+" "+data.results[0].name.last);
+  			$("#imgFoto").attr("src",data.results[0].picture.large);
+    		console.log(data.results[0].name.first+" "+data.results[0].name.last);
+	  }
+		});
 	}
-	$("#dameClic").on("clic",dameclic);
+	$("#dameClic").on("click",dameClic);
 }
 
 
