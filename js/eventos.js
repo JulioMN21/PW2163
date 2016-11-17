dfb953b91295184408ea678224c8e97ec1ad42b9
CCDM15 <ccdm15@iMac-de-CCDM15.local>
2016-11-17 11:53:37 -0700

    Segundo avance del JQUERY con CSS

--- js/eventos.js
+++ js/eventos.js
@@ -8,9 +8,13 @@ var inicio = function() //main
   		url: 'https://randomuser.me/api/',
   		dataType: 'json',
   		success: function(data) {
+  			$("#imgFoto").show("slow");
+  			$("#txtNombre").show("slow");
   			$("#txtNombre").val(data.results[0].name.first+" "+data.results[0].name.last);
   			$("#imgFoto").attr("src",data.results[0].picture.large);
+  			$("#miArticle").html("Texto").show();
     		console.log(data.results[0].name.first+" "+data.results[0].name.last);
+
 	  }
 		});
 	}
