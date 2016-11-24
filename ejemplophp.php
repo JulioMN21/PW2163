2b21fafa5bf288b772a5189bc75cab8f9259f65a
CCDM15 <ccdm15@iMac-de-CCDM15.local>
2016-11-24 11:57:26 -0700

    Registro_Ejemplo PHP

--- ejemplophp.php
+++ ejemplophp.php
@@ -1,71 +1,6 @@
-<?php 
- 	//Preguntar si los valores existen
-	$oculto = ""; //inicializando la variable
-	$usuario = "";
-	$nombre = "";
-	$clave = "";
-	$tipo = "";
-	
-	if (isset($_POST["txtOculto"])) //si trae valores
-	{
-		$oculto =  $_POST["txtOculto"];
-	}
-
-	if (isset($_POST["txtUsuario"])) //si trae valores
-	{
-		$oculto =  $_POST["txtUsuario"];
-	}
-
-	if (isset($_POST["txtNombre"])) //si trae valores
-	{
-		$oculto =  $_POST["txtNombre"];
-	}
-
-	if (isset($_POST["txtClave"])) //si trae valores
-	{
-		$oculto =  $_POST["txtClave"];
-	}
-
-	if (isset($_POST["txtTipo"])) //si trae valores
-	{
-		$oculto =  $_POST["txtTipo"];
-	}
-
-	function guardaUsusario($usuario,$nombre,$clave,$tipo)
-	{
-		//conectarse al servidor de MySQL
-		//mysql_connect (servidor,usuario,contraseña)
-		$conexion = mysql_connect("localhost","root","")
-		//seleccionamos la Base de Datos
-		mysql_select_db("bd2163");
-		$consulta = "insert into usuarios values('".$usuario."','".$nombre."','".$clave."','".$tipo."')"
-		//Ejecutamos la consulta
-		mysql_query($consulta);
-		//Preguntamos si hubi inserción
-		if (msql_affected_rows() > 0) 
-		{
-			print "Registro guardado";
-		}
-		else
-		{
-			print "No se pudo guardar el registro":
-		}
-	}
-
-	switch ($oculto) {
-		case 'guardaUsuario':
-			guardaUsuario($usuario,$nombre,$clave,$tipo)
-
-			break;
-		
-		default:
-			# code...
-			break;
-	}
- ?>
 
 <h1>Alta de Ususarios</h1>
-<form action="ejemplo.php" method="POST">
+<form action="registro_ejemplo.php" method="POST">
 	<input type="hidden" name="txtOculto" value="guardaUsuario"> <br>
 	<input type="text" name="txtUsuario" id="txtUsuario"> <br>
 	<input type="text" name="txtNombre" id="txtNombre"> <br>
@@ -77,7 +12,26 @@
 	</select>
 	<input type="submit" Value="Enviar">
 </form>
+<hr>
 <?php
-
+//Conecto al servidor
+	$conexion = mysql_connect("localhost","root","")
+	mysql_select_db("bd2163");
+	$consulta = "select * from usuarios order by usuario";
+	$resultado = mysql_query($consulta); //Ejecutando consulta
+	$tabla = "<table border=1";
+	$tabla = "<tr>";
+	$tabla = "<th>Usuario</th><th>Nombre</th><th>Clave</th><th>Tipo</th>"
+	$tabla = "</tr>"
+	while ($registro = mysql_fetch_array($resultado)) 
+	{
+		$tabla = "<tr>";
+		$tabla = "<td>".$registro["usuario"]."</td>";
+		$tabla = "<td>".$registro["nombre"]."</td>";
+		$tabla = "<td>".$registro["clave"]."</td>";
+		$tabla = "<td>".$registro["tipo"]."</td>";
+		$tabla = "</tr>";
+	}
+	$tabla.="</table>";
+	print $table;
 ?>
-
